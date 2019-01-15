ab86f6cda2e7f01d267aa9d9c70867e9e9b89456
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-01-15 03:08:39 +0300

    Add travis config

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 import argparse
 import tracemalloc
+import sys
 
 import leaky
 
@@ -17,10 +18,18 @@ elif args.func == 'leak_int':
     callee = leaky.leak_int
 elif args.func == 'doublefree_int':
     callee = leaky.doublefree_int
+elif args.func == 'leak_none':
+    callee = leaky.leak_none
+elif args.func == 'doublefree_none':
+    callee = leaky.doublefree_none
 elif args.func == 'leak_malloc':
     callee = leaky.leak_malloc
 elif args.func == 'doublefree_malloc':
     callee = leaky.doublefree_malloc
+elif args.func == 'leak_pymem':
+    callee = leaky.leak_pymem
+elif args.func == 'doublefree_pymem':
+    callee = leaky.doublefree_pymem
 else:
     raise RuntimeError('unknown func {}'.format(args.func))
 
@@ -31,11 +40,18 @@ def tracemalloc_wrapper(callee):
     callee()
     snapshot2 = tracemalloc.take_snapshot()
 
-    top_stats = snapshot2.compare_to(snapshot1, 'lineno')
+    filters = (tracemalloc.Filter(False, tracemalloc.__file__),)
+    snapshot1 = snapshot1.filter_traces(filters)
+    snapshot2 = snapshot2.filter_traces(filters)
 
-    print("tracemalloc stats:")
-    for stat in top_stats:
-        print(stat)
+    leaks = False
+    for stat in snapshot2.compare_to(snapshot1, 'lineno'):
+        if stat.size_diff != 0:
+            print(stat)
+            leaks = True
+
+    if leaks:
+        sys.exit(1)
 
 if args.tracemalloc:
     tracemalloc_wrapper(callee)
--- src/leaky.c
+++ src/leaky.c
@@ -24,26 +24,17 @@
 
 #include <stdlib.h>
 
-static PyObject* no_leak(PyObject *self, PyObject *args) {
-	(void)self; // (unused)
-	(void)args; // (unused)
-
+static PyObject* no_leak(PyObject* self, PyObject* args) {
 	Py_RETURN_NONE;
 }
 
-static PyObject* leak_int(PyObject *self, PyObject *args) {
-	(void)self; // (unused)
-	(void)args; // (unused)
-
+static PyObject* leak_int(PyObject* self, PyObject* args) {
 	PyLong_FromLong(0x876543210);
 
 	Py_RETURN_NONE;
 }
 
-static PyObject* doublefree_int(PyObject *self, PyObject *args) {
-	(void)self; // (unused)
-	(void)args; // (unused)
-
+static PyObject* doublefree_int(PyObject* self, PyObject* args) {
 	PyObject* o = PyLong_FromLong(0x876543210);
 	Py_DECREF(o);
 	Py_DECREF(o);
@@ -51,19 +42,25 @@ static PyObject* doublefree_int(PyObject *self, PyObject *args) {
 	Py_RETURN_NONE;
 }
 
-static PyObject* leak_malloc(PyObject *self, PyObject *args) {
-	(void)self; // (unused)
-	(void)args; // (unused)
-
-	(void)malloc(1024);
+static PyObject* leak_none(PyObject* self, PyObject* args) {
+	Py_INCREF(Py_None);
 
 	Py_RETURN_NONE;
 }
 
-static PyObject* doublefree_malloc(PyObject *self, PyObject *args) {
-	(void)self; // (unused)
-	(void)args; // (unused)
+static PyObject* doublefree_none(PyObject* self, PyObject* args) {
+	Py_DECREF(Py_None);
 
+	Py_RETURN_NONE;
+}
+
+static PyObject* leak_malloc(PyObject* self, PyObject* args) {
+	malloc(1024);
+
+	Py_RETURN_NONE;
+}
+
+static PyObject* doublefree_malloc(PyObject* self, PyObject* args) {
 	void* p = malloc(1024);
 	free(p);
 	free(p);
@@ -71,12 +68,30 @@ static PyObject* doublefree_malloc(PyObject *self, PyObject *args) {
 	Py_RETURN_NONE;
 }
 
+static PyObject* leak_pymem(PyObject* self, PyObject* args) {
+	PyMem_Malloc(1024);
+
+	Py_RETURN_NONE;
+}
+
+static PyObject* doublefree_pymem(PyObject* self, PyObject* args) {
+	void* p = PyMem_Malloc(1024);
+	PyMem_Free(p);
+	PyMem_Free(p);
+
+	Py_RETURN_NONE;
+}
+
 static PyMethodDef module_methods[] = {
-	{"no_leak", (PyCFunction)no_leak, METH_VARARGS, ""},
-	{"leak_int", (PyCFunction)leak_int, METH_VARARGS, ""},
-	{"doublefree_int", (PyCFunction)doublefree_int, METH_VARARGS, ""},
-	{"leak_malloc", (PyCFunction)leak_malloc, METH_VARARGS, ""},
-	{"doublefree_malloc", (PyCFunction)doublefree_malloc, METH_VARARGS, ""},
+	{"no_leak", no_leak, METH_VARARGS, ""},
+	{"leak_int", leak_int, METH_VARARGS, ""},
+	{"doublefree_int", doublefree_int, METH_VARARGS, ""},
+	{"leak_none", leak_none, METH_VARARGS, ""},
+	{"doublefree_none", leak_none, METH_VARARGS, ""},
+	{"leak_malloc", leak_malloc, METH_VARARGS, ""},
+	{"doublefree_malloc", doublefree_malloc, METH_VARARGS, ""},
+	{"leak_pymem", leak_malloc, METH_VARARGS, ""},
+	{"doublefree_pymem", doublefree_malloc, METH_VARARGS, ""},
 	{NULL, NULL, 0, NULL}
 };
 
